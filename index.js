18ecf8ede3fd3f3f3ca3fce6e4d83312b7d66425
AbdalrahmanM <abdulrahman.al-samaraie@ogr.sakarya.edu.tr>
2023-04-10 23:15:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(gifts,name){
+    const thankArray=[];
+    for(let i=0;i<gifts.length;i++){
+        thankArray.push(`Thank you, ${gifts[i]}, for the wonderful ${name} gift!`)
+    }
+    return thankArray;
+}
+function countDown(number){
+    
+    while (number >= 0) {
+        console.log(number--);
+      }
+}
